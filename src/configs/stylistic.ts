4190bdcb665ff4b74af2788db2ce47bf78cca378
j10c <blyb1739@gmail.com>
2024-11-17 19:35:43 +0800

    feat(lint): lint config for stylistic

---
+++ src/configs/stylistic.ts
@@ -0,0 +1,45 @@
+import { Linter } from "eslint";
+import pluginStylistic from "@stylistic/eslint-plugin";
+
+export default function stylistic() {
+
+  return [
+    {
+      name: "zjutjh/stylistic/rules",
+      plugins: {
+        "@stylistic": pluginStylistic
+      },
+      rules: {
+        "@stylistic/indent": ["error", 2],
+        "@stylistic/keyword-spacing": "error",
+        "@stylistic/key-spacing": "error",
+        "@stylistic/no-trailing-spaces": "error",
+        "@stylistic/linebreak-style": ["error", "unix"],
+        "@stylistic/quotes": ["error", "double"],
+        "@stylistic/function-call-spacing": "error",
+        "@stylistic/semi": "error",
+        "@stylistic/no-multiple-empty-lines": ["warn", { max: 1 }],
+        "@stylistic/object-curly-spacing": ["error", "always"],
+        "@stylistic/arrow-spacing": "error",
+        "@stylistic/block-spacing": "error",
+        "@stylistic/brace-style": "error",
+        "@stylistic/comma-dangle": "error",
+        "@stylistic/no-multi-spaces": "error",
+        "@stylistic/comma-spacing": "error",
+        "@stylistic/switch-colon-spacing": "error",
+        "@stylistic/type-annotation-spacing": "error",
+
+        "@stylistic/space-before-blocks": "error",
+        "@stylistic/space-before-function-paren": ["error", {
+          "anonymous": "never",
+          "named": "never",
+          "asyncArrow": "always"
+        }],
+        "@stylistic/space-in-parens": "error",
+        "@stylistic/space-infix-ops": "error",
+        "@stylistic/spaced-comment": "error",
+      }
+    }
+
+  ] satisfies Linter.Config[];
+}
--- src/factory.ts
+++ src/factory.ts
@@ -1,10 +1,12 @@
 import { Linter } from "eslint";
 import javascript from "./configs/javascript";
+import stylistic from "./configs/stylistic";
 
 export default async function zjutjh() {
   const configs: Linter.Config[][] = [];
 
   configs.push(javascript());
+  configs.push(stylistic());
 
   return configs.flat(1) satisfies Linter.Config[];
 }
