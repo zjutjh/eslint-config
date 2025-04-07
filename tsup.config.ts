dd8e376fb512517798f77f645fa07185afecec80
j10c <blyb1739@gmail.com>
2025-04-07 23:57:00 +0800

    chore(build): simplify tsup configuration and build script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/index.ts"
-  ],
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  clean: true,
+  dts: true,
   shims: true
 });
