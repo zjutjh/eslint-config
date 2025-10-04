9d04639979ab72309dda81b1d89ef92b861f44c8
j10c <blyb1739@gmail.com>
2025-10-04 18:50:26 +0800

    feat(lint/import): combine same import source

--- src/configs/imports.ts
+++ src/configs/imports.ts
@@ -1,4 +1,5 @@
 import { Linter } from "eslint";
+import eslintImportPlugin from "eslint-plugin-import";
 import simpleImportSortPlugin from "eslint-plugin-simple-import-sort";
 
 export default function imports(): Linter.Config[] {
@@ -7,14 +8,16 @@ export default function imports(): Linter.Config[] {
     {
       name: "zjutjh/imports/setup",
       plugins: {
-        "simple-import-sort": simpleImportSortPlugin
+        "simple-import-sort": simpleImportSortPlugin,
+        "import": eslintImportPlugin
       }
     },
     {
       name: "zjutjh/imports/rules",
       rules: {
         "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error"
+        "simple-import-sort/exports": "error",
+        "import/no-duplicates": "error"
       }
     }
   ];
